cae1e170718d348d9b6d5a602445dff9c13fe049
Carlos Schiavi <cdcarlos@gmail.com>
2024-11-30 18:54:43 -0300

    Adiciona diretorio pages

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Só tenho a agradecer a cada um dos desafios que me trouxeram até aqui, que me fizeram crescer e aprender tantas coisas!</h1>
+}
+
+export default Home;
