008484ce7ca5b80061a2cd38874d1ba060ee332b
Carlos Schiavi <45151675+cschiavi@users.noreply.github.com>
2025-01-04 19:18:13 +0000

    Adicionado configuracoes do prettier, `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <p align="center" ><h1>schiavi.dev.br</h1></p>
-}    
+function Home() {
+  return (
+    <p align="center">
+      <h1>schiavi.dev.br</h1>
+    </p>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
 
 export default Home;
